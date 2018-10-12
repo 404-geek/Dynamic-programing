8c7b15b13d3197c50648a1316b25a860efc4fc0c
Pranay Das <pranayd61@gmail.com>
2018-10-13 02:17:22 +0530

    changes

--- coneven.py
+++ coneven.py
@@ -1,32 +1,82 @@
 # Dynamic-programing
-stre = "aabbccbbdccde"
+stre = "aaabbbccbbdccde"
+# stre = "dde"
 
-data = list(stre)
-temp = list(stre)
-i=j=count=0
+b = list(stre)
+n = list(stre)
 
-while i < len(temp):
-    k = i
-    print(data[j]+"     "+temp[i]+"      "+str(j)+"     "+str(i))
 
-    if data[j] == temp[i]:
-        count+=1
-        print("Count="+str(count))
-        i+=1
-    else:
-        print("Inside Else")
+def ret(data,temp):
+    i = j = count = 0
+    while i <= len(temp):
+        if(i==len(temp)):
+            # print("inside equals")
+            # print(data)
+            ret(data,data)
+            break
+        k = i
+        # print(data[j]+"     "+temp[i]+"      "+str(j)+"     "+str(i))
 
-        if count%2 == 0:
-            print("Inside if even")
-            del data[j:j+count]
-            count = 0
-            j = data.index(temp[i])
-            i = k
+        if data[j] == temp[i]:
+            count+=1
+            # print("Count="+str(count))
+            i+=1
+
         else:
-            print("Inside if odd")
-            i = k
-            j = count
-            count = 0
+            # print("Inside Else")
+
+            if count%2 == 0:
+                # print("Inside if even")
+                del data[j:j+count]
+                if(len(data)==1):
+                    return data
+                    break
+                count = 0
+                j = data.index(temp[i])
+                i = k
+            else:
+                # print("Inside if odd")
+                i = k
+                j = count
+                count = 0
+
+    return data
+
+
+print(ret(b,n))
+
+
+#
+# data = list(stre)
+# temp = list(stre)
+# i=j=count=0
+#
+# while i < len(temp):
+#     k = i
+#     print(data[j]+"     "+temp[i]+"      "+str(j)+"     "+str(i))
+#
+#     if data[j] == temp[i]:
+#         count+=1
+#         print("Count="+str(count))
+#         i+=1
+#     else:
+#         print("Inside Else")
+#
+#         if count%2 == 0:
+#             print("Inside if even")
+#             print (str(j)+"     "+str(j+count))
+#             del data[j:j+count]
+#             print (data)
+#             count = 0
+#             j = data.index(temp[i])
+#             i = k
+#         else:
+#             print("Inside if odd")
+#             i = k
+#             j = count
+#             count = 0
+#
+# # print(data)
 
 
 
