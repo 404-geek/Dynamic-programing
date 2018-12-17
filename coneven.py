8bc53686f0379cf2531a72c4c3121daf1c7c071a
Pranay Das <pranayd61@gmail.com>
2018-12-17 12:56:27 +0530

    Update coneven.py

--- coneven.py
+++ coneven.py
@@ -48,38 +48,5 @@ def ret(data,temp):
 print(ret(b,n))
 
 
-#
-# data = list(stre)
-# temp = list(stre)
-# i=j=count=0
-#
-# while i < len(temp):
-#     k = i
-#     print(data[j]+"     "+temp[i]+"      "+str(j)+"     "+str(i))
-#
-#     if data[j] == temp[i]:
-#         count+=1
-#         print("Count="+str(count))
-#         i+=1
-#     else:
-#         print("Inside Else")
-#
-#         if count%2 == 0:
-#             print("Inside if even")
-#             print (str(j)+"     "+str(j+count))
-#             del data[j:j+count]
-#             print (data)
-#             count = 0
-#             j = data.index(temp[i])
-#             i = k
-#         else:
-#             print("Inside if odd")
-#             i = k
-#             j = count
-#             count = 0
-#
-# # print(data)
-
-
 
 
